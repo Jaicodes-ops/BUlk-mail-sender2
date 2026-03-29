addcca16fc71c639e3fe58dad4d654ccbe3280f9
jai <j.jayaraj1556@gmail.com>
2026-03-30 00:06:07 +0530

    commit1

--- backend/index.js
+++ backend/index.js
@@ -8,48 +8,51 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb+srv://jayarajraj81:DGziHRyy0uyYWqSX@cluster0.6apn4ev.mongodb.net/passkey?appName=Cluster0")
+  .connect(
+    "mongodb+srv://jayarajraj81:DGziHRyy0uyYWqSX@cluster0.6apn4ev.mongodb.net/passkey?appName=Cluster0",
+  )
   .then(() => console.log("connected to db"))
   .catch(() => console.log("failed to connect"));
 
-const credential = mongoose.model("credential", {}, "bulkmail");
+const credentialSchema = new mongoose.Schema(
+  { user: String, pass: String },
+  { strict: false },
+);
+const credential = mongoose.model("credential", credentialSchema, "bulkmail");
 
 app.post("/sendmail", async (req, res) => {
-  res.send(true); // respond fast
-
-  const msg = req.body.msg;
-  const emaillist = req.body.emaillist;
+  const { msg, emaillist } = req.body;
 
   try {
     const data = await credential.find();
 
+    if (!data || data.length === 0) {
+      return res.send(false);
+    }
+
+    const { user, pass } = data[0].toJSON();
+
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      auth: {
-        user: data[0].user,
-        pass: data[0].pass,
-      },
+      auth: { user, pass },
     });
 
     for (let i = 0; i < emaillist.length; i++) {
-      try {
-        await transporter.sendMail({
-          from: "jayarajraj81@gmail.com",
-          to: emaillist[i],
-          subject: "this message from bulk mail app",
-          text: msg,
-        });
-
-        console.log("email sent to:", emaillist[i]);
-      } catch (err) {
-        console.log("failed:", emaillist[i]);
-      }
+      await transporter.sendMail({
+        from: "jayarajraj81@gmail.com",
+        to: emaillist[i],
+        subject: "this message from bulk mail app",
+        text: msg,
+      });
+      console.log("email sent to: " + emaillist[i]);
     }
 
+    res.send(true);
   } catch (error) {
-    console.log("error:", error);
+    console.log(error);
+    res.send(false);
   }
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on", PORT));
+app.listen(PORT, () => console.log("Server running"));
